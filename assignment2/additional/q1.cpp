8a83d3ee1d0a5ceb13a7488695c740914259fbee
kpoplibe24-netizen <kpopli_be24@thapar.edu>
2025-09-05 12:43:56 +0530

    Create q1.cpp

---
+++ assignment2/additional/q1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n, k;
+    cin >> n >> k;
+    int arr[n];
+    for(int i=0;i<n;i++) cin >> arr[i];
+    int count=0;
+    for(int i=0;i<n;i++) {
+        for(int j=i+1;j<n;j++) {
+            if(abs(arr[i]-arr[j])==k) count++;
+        }
+    }
+    cout << count;
+}
