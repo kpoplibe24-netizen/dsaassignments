3e586a91cc624459e12a62a6d7540d84747da06b
kpoplibe24-netizen <kpopli_be24@thapar.edu>
2025-09-05 13:03:07 +0530

    Create q3.cpp

---
+++ assignment3/q3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool isBalanced(string expr) {
+    stack<char> st;
+    for(char c : expr) {
+        if(c=='(' || c=='{' || c=='[') st.push(c);
+        else if(c==')' || c=='}' || c==']') {
+            if(st.empty()) return false;
+            char top = st.top(); st.pop();
+            if((c==')' && top!='(') ||
+               (c=='}' && top!='{') ||
+               (c==']' && top!='[')) return false;
+        }
+    }
+    return st.empty();
+}
+
+int main() {
+    string expr;
+    cin >> expr;
+    if(isBalanced(expr)) cout << "Balanced";
+    else cout << "Not Balanced";
+}
