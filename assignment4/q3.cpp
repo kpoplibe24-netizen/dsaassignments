e368c502e9cd495dcd3949fb3cdee242845d8f95
kpoplibe24-netizen <kpopli_be24@thapar.edu>
2025-09-19 18:56:55 +0530

    Create q3.cpp

---
+++ assignment4/q3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+void interleaveQueue(queue<int>& q) {
+    int n = q.size();
+    if (n % 2 != 0) {
+        cout << "Queue size must be even!\n";
+        return;
+    }
+    queue<int> firstHalf;
+    for (int i = 0; i < n/2; i++) {
+        firstHalf.push(q.front()); q.pop();
+    }
+
+    while (!firstHalf.empty()) {
+        q.push(firstHalf.front()); firstHalf.pop();
+        q.push(q.front()); q.pop();
+    }
+}
+
+int main() {
+    queue<int> q;
+    q.push(4); q.push(7); q.push(11); q.push(20); q.push(5); q.push(9);
+
+    interleaveQueue(q);
+
+    cout << "Output: ";
+    while (!q.empty()) { cout << q.front() << " "; q.pop(); }
+    cout << endl;
+}
