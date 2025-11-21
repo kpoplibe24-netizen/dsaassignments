8370bdcdaad17c5a7b675345d5c1e601ced31471
kpoplibe24-netizen <kpopli_be24@thapar.edu>
2025-11-21 14:25:55 +0530

    Create q1(a).cpp

---
+++ assignment7/q1(a).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    int arr[100];
+    cout << "Enter numbers: ";
+    for(int i=0;i<n;i++) cin >> arr[i];
+
+    for(int i=0;i<n;i++){
+        int min = i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j] < arr[min]) min = j;
+        }
+        int temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+
+    cout << "Selection Sort: ";
+    for(int i=0;i<n;i++) cout << arr[i] << " ";
+    cout << endl;
+}
