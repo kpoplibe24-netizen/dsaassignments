7d707a5a5691d13af1f624fe142be55f8efdba87
kpoplibe24-netizen <kpopli_be24@thapar.edu>
2025-11-21 14:19:57 +0530

    Create q2.cpp

---
+++ assignment6/homework/q2.cpp
@@ -0,0 +1,95 @@
+#include<iostream>
+using namespace std;
+class Node{
+public:
+int data;
+Node* next;
+Node(int val){
+  data = val;
+  next = NULL;
+}
+};
+class CircularList{
+Node* head;
+Node* tail;
+public:
+CircularList(){
+  head=tail = NULL;
+}
+void insertAtBeginning(int val){
+  Node* newNode = new Node(val);
+  if(head==NULL){
+    head=tail=newNode;
+    tail->next = head;
+  }
+  else{
+    newNode->next = head;
+    head = newNode;
+    tail->next = head;
+  }
+}
+void insertAtTail(int val){
+  Node* newNode = new Node(val);
+  if(head==NULL){
+    head = tail = newNode;
+    tail->next = head;
+  }
+  else{
+    newNode->next = head;
+    tail->next = newNode;
+    tail = newNode;
+  }
+}
+void deleteAtHead(){
+  if(head==NULL) return;
+  else if(head==tail){
+    delete head;
+    head = tail = NULL;
+  }
+  else{
+    Node* temp = head;
+    head = head->next;
+    tail->next = head;
+    temp->next = NULL;
+    delete temp;
+  }
+  
+}
+void deleteAtTail(){
+  if(head==NULL) return;
+  else if(head==tail){
+    delete head;
+    head = tail = NULL;
+  }
+  else{
+    Node* temp = tail;
+    Node* prev = head;
+    while(prev->next!=tail){
+      prev = prev->next;
+    }
+    tail = prev;
+    tail->next = head;
+    temp->next = NULL;
+    delete temp;
+  }
+}
+void printList(){
+  if(head==NULL) return;
+  cout<<head->data<<"->";
+  Node* temp = head->next;
+  while(temp!=head){
+    cout<<temp->data<<"->";
+    temp = temp->next;
+  }
+  cout<<head->data<<endl;
+}
+};
+int main(){
+  CircularList cll;
+  cll.insertAtBeginning(60);
+  cll.insertAtBeginning(80);
+  cll.insertAtBeginning(40);
+  cll.insertAtBeginning(100);
+  cll.insertAtBeginning(20);
+  cll.printList();
+}
