5ba341c5d0a62297d85ca68f8aef6e969cae5650
kpoplibe24-netizen <kpopli_be24@thapar.edu>
2025-12-05 01:26:28 +0530

    Create q6.cpp

---
+++ assignment8/q6.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class MinHeap{
+    vector<int>heap;
+    void heapifyUp(int i){
+        while(i>0){
+            int p=(i-1)/2;
+            if(heap[p]>heap[i]){
+                swap(heap[p],heap[i]);
+                i=p;
+            }else break;
+        }
+    }
+    void heapifyDown(int i){
+        int n=heap.size();
+        while(true){
+            int l=2*i+1;
+            int r=2*i+2;
+            int s=i;
+            if(l<n&&heap[l]<heap[s])s=l;
+            if(r<n&&heap[r]<heap[s])s=r;
+            if(s!=i){
+                swap(heap[i],heap[s]);
+                i=s;
+            }else break;
+        }
+    }
+public:
+    void push(int x){
+        heap.push_back(x);
+        heapifyUp(heap.size()-1);
+    }
+    void pop(){
+        if(heap.empty())return;
+        heap[0]=heap.back();
+        heap.pop_back();
+        if(!heap.empty())heapifyDown(0);
+    }
+    int top(){
+        if(heap.empty())throw runtime_error("empty");
+        return heap[0];
+    }
+    bool empty(){
+        return heap.empty();
+    }
+    void print(){
+        for(int x:heap)cout<<x<<" ";
+        cout<<"\n";
+    }
+};
+
+int main(){
+    MinHeap pq;
+    pq.push(5);
+    pq.push(3);
+    pq.push(8);
+    pq.push(1);
+    pq.print();
+    cout<<pq.top()<<"\n";
+    pq.pop();
+    cout<<pq.top()<<"\n";
+    pq.print();
+}
