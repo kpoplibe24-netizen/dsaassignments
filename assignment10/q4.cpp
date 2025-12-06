5c2de96e8e684fdfad4225aeca86ad4e3451db57
kpoplibe24-netizen <kpopli_be24@thapar.edu>
2025-12-07 02:22:52 +0530

    Create q4.cpp

---
+++ assignment10/q4.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+#define MAX 100000
+
+void find(int arr[], int n){
+    int hash[MAX]={0};
+    for(int i=0;i<n;i++){
+        hash[arr[i]]++;
+    }
+
+    for(int i=0;i<n;i++){
+        if(hash[arr[i]]==1){
+            cout<<arr[i];
+            break;
+        }
+    }
+
+}
+int main(){
+    int arr[]={4,5,1,2,0,4};
+    int n=sizeof(arr)/sizeof(arr[0]);
+
+    find(arr,n);
+}
